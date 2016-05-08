8fd88261698cd1ee499b347965df17dfa3cf6b7f
Brian Danowski <briandanowski@gmail.com>
2016-05-08 10:52:51 -0400

    added fix functions and returns lang and ip now

--- routes/whoami.js
+++ routes/whoami.js
@@ -4,10 +4,23 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log(req.ip);
-  //console.log(req.headers['x-forwarded-for']);
-  //console.log(req.conection);
-  res.json(req.ip);
+
+  console.log(req.headers["accept-language"]);
+  var templateObj = {
+    ipaddress: fixIp(req.ip),
+    language: fixLanguage(req.headers["accept-language"])
+  };
+  res.json(templateObj);
 
 });
+
+var fixIp = function(){
+  return ip.split(':').reverse()[0];
+};
+
+
+var fixLanguage = function(lang){
+  return lang.split(,)[0];
+};
 
 module.exports = router;
