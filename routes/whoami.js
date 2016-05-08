a9576d5f0677728e7bf6705e7b2bf37006305e78
Brian Danowski <briandanowski@gmail.com>
2016-05-08 11:05:27 -0400

    added useragent middleware and whoami api now returns os

--- routes/whoami.js
+++ routes/whoami.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+var useragent = require('express-useragent');
 /* GET users listing. */
+
+router.use(useragent.express());
+
 router.get('/', function(req, res, next) {
   console.log(req.ip);
 
   console.log(req.headers["accept-language"]);
   var templateObj = {
     ipaddress: fixIp(req.ip),
-    language: fixLanguage(req.headers["accept-language"])
+    language: fixLanguage(req.headers["accept-language"]),
+    software: req.useragent.os
   };
   res.json(templateObj);
 
